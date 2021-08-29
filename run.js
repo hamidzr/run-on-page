e0d8882978155ef447253505a93aef6156342d80
Hamid Zare <hamidzare.vu@gmail.com>
2021-08-29 15:04:59 -0700

    init

---
+++ run.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+/*
+Run a source code on web page. The source code must define a main function with no parameters.
+*/
+
+const puppeteer = require('puppeteer');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+const fs = require('fs');
+
+let srcCode = '';
+if (argv.srcPath) {
+  srcCode = fs.readFileSync(argv.srcPath).toString();
+} else if (argv.src) {
+  srcCode = argv.src;
+} else {
+  process.exit(1);
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = (await browser.pages())[0];
+  // const page = await browser.newPage();
+  await page.goto(argv.url);
+  await page.addScriptTag({content: srcCode});
+  page.on('dialog', async dialog => {
+    await dialog.dismiss();
+  })
+  res = await page.evaluate(() => main());
+  console.log(res);
+  await browser.close()
+})()
